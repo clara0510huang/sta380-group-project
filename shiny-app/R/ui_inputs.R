b1ef3594a1abe4c2ab97332acbf9ad29b84c244f
clara0510huang <116289805+clara0510huang@users.noreply.github.com>
2026-03-12 16:53:29 -0400

    Implement the selection of the output part

--- shiny-app/R/ui_inputs.R
+++ shiny-app/R/ui_inputs.R
@@ -15,5 +15,17 @@ my_inputs <- tagList(
   #4
   numericInput("seed_val", "Seed:", value = 2000),
   #5
-  actionButton("run_btn", "Run Analysis!:", class = "btn-primary", style = "width: 100%; margin-top: 15px;")
+  actionButton("run_btn", "Run Analysis!:", class = "btn-primary", style = "width: 100%; margin-top: 15px;"),
+  #6 checkbox select output
+  checkboxGroupInput("info_present", "Select Output to Show",
+                     choices = c(
+                       "Scatter Plot (LR vs Bootstrap)" = "Scatter Plot",
+                       "Bootstrap Summary Table" = "Summary",
+                       "Correlation Histogram" = "Correlation Histogram",
+                       "Bootstrap Histogram (Intercept/Slope)" = "Bootstrap Histogram",
+                       "CI Comparison (Bootstrap vs OLS)" = "CI",
+                       "IQR Boxplot" = "IQR"
+                     ),
+                     selected = c("Scatter Plot","Correlation Histogram", "Bootstrap Histogram","Summary", "CI", "IQR")),
 )
+
